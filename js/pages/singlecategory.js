0d4ce426b5749e2e61cc28586d30b1cf49fa07e7
Пол Касуса <65332516+can7wysiwyg@users.noreply.github.com>
2025-06-20 10:35:06 +0200

    urgent changes

---
+++ js/pages/singlecategory.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', async() => {
+
+    try {
+        
+            const catId = window.location.hash.substring(1); 
+    
+
+        
+    } catch (error) {
+        console.log("There was a problem", error.message)
+    }
+
+
+})
